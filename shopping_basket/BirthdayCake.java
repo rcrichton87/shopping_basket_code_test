661dc2ba4090555ebebd286a4ba16c85996d2ece
Ross Crichton <rcrichton87@googlemail.com>
2017-03-31 13:40:12 +0100

    added BirthdayCake class implementing the item interface

---
+++ shopping_basket/BirthdayCake.java
@@ -0,0 +1,27 @@
+package shopping_basket;
+
+public class BirthdayCake implements Item{
+
+  String name;
+  int price;
+  boolean bogof;
+
+  public Apples(){
+    this.name = "Birthday Cake";
+    this.price = 1000;
+    this.bogof = false;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int getPrice(){
+    return this.price;
+  }
+
+  public boolean isBOGOF(){
+    return this.bogof;
+  }
+
+}
